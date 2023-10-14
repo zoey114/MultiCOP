e83ebb8de6d3e2e1fed533e19a45fc244617cd7a
Zoey <56131629+zoey114@users.noreply.github.com>
2023-10-14 05:22:46 -0400

    Update main.R

--- code/R/main.R
+++ code/R/main.R
@@ -4,19 +4,11 @@ library(MASS)
 library(dr)
 source("R/utils.R")
 
-scalar.Y = scalar.y(Y, m=m)
-cl <- makeCluster(detectCores())
-registerDoParallel(cl)
-result.X <- foreach(i = 1:ncol(scalar.Y)) %dopar% {
-  step.multicop.x(i)
-}
-stopCluster(cl)
-X_sel_final = select.idx(result.X)
-scalar.X = scalar.x(X.selected, m=1000)
-cl <- makeCluster(detectCores())
-registerDoParallel(cl)
-result.Y <- foreach(i = 1:ncol(scalar.X)) %dopar% {
-  step.multicop.y(i)
-}
-stopCluster(cl)
-Y_sel_final = select.idx(result.Y)
+scalar.Y = scalar(Y, m=m)
+result.X = lapply(1:m, function(i){suppressWarnings(step.multicop.x(i))})
+X_sub = select.idx(result.X)
+
+X.selected = X[,as.numeric(X_sub)]
+scalar.X = scalar(X.selected, m)
+result.Y = lapply(1:m, function(i){suppressWarnings(step.multicop.y(i))})
+Y_sub = select.idx(result.Y) 

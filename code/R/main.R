87762b6845404b011c070db171481ec9ca8c3cce
Zoey <56131629+zoey114@users.noreply.github.com>
2023-10-14 05:34:54 -0400

    Update main.R

--- code/R/main.R
+++ code/R/main.R
@@ -4,11 +4,16 @@ library(MASS)
 library(dr)
 source("R/utils.R")
 
-scalar.Y = scalar(Y, m=m)
-result.X = lapply(1:m, function(i){suppressWarnings(step.multicop.x(i))})
-X_sub = select.idx(result.X)
-
-X.selected = X[,as.numeric(X_sub)]
-scalar.X = scalar(X.selected, m)
-result.Y = lapply(1:m, function(i){suppressWarnings(step.multicop.y(i))})
-Y_sub = select.idx(result.Y) 
+multicop = function(alpha.in.list, alpha.out.list, m, my.range){
+  sub = list()
+  scalar.Y = scalar(Y, m=m)
+  result.X = lapply(1:m, function(i){suppressWarnings(step.multicop.x(i))})
+  X_sub = select.idx(result.X)
+  X.selected = X[,as.numeric(X_sub)]
+  scalar.X = scalar(X.selected, m)
+  result.Y = lapply(1:m, function(i){suppressWarnings(step.multicop.y(i))})
+  Y_sub = select.idx(result.Y) 
+  sub[[1]] = X_sub
+  sub[[2]] = Y_sub
+  return(sub)
+}
